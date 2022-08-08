ed80fd401db667cfdc072d21967835aa7200b03d
lalaina-ra <nomenalalaina.randriamarovelo@gmail.com>
2022-08-08 16:38:21 +0200

    update project

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+//import logo from '../assets/logo.png';
+import '../styles/Banner.css';
+
+
+function Banner({ children }) {
+    //const title = 'La maison jungle'
+    return <div className='lmj-banner'>{children}</div>
+}
+
+export default Banner
